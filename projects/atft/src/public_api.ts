a59ff9dd1b3fb2718f51c0f0565a4d62ed631e75
ComFreek <comfreek@outlook.com>
2019-04-17 16:22:13 +0200

    Test raycaster for issue #36

--- projects/atft/src/public_api.ts
+++ projects/atft/src/public_api.ts
@@ -3,3 +3,5 @@
  */
 
 export * from './lib/atft.module';
+export * from './lib/cameras/perspective-camera.directive';
+export * from './lib/objects/scene.directive';
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
+import { PerspectiveCameraDirective, SceneDirective } from 'atft';
+
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +16,35 @@ export class AppComponent {
   public rotationZ = 0.0;
 
   public translationY = 0.0;
+
+  private raycaster = new THREE.Raycaster();
+
+  @ViewChild(PerspectiveCameraDirective)
+  private cameraDirective: PerspectiveCameraDirective;
+
+  @ViewChild(SceneDirective)
+  private sceneDirective: SceneDirective;
+
+  private mousePosition = new THREE.Vector2();
+
+  @HostListener('mousemove', ['$event'])
+  private captureMousePosition(event: MouseEvent) {
+    event.preventDefault();
+    this.mousePosition.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    this.mousePosition.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+  }
+
+  @HostListener('click')
+  private shoutClickedObject() {
+    this.raycaster.setFromCamera(this.mousePosition, this.cameraDirective.camera);
+
+    console.log(((this.sceneDirective as any).object as THREE.Scene)
+    .children.map(x => x.type));
+    const relevantObjects = ((this.sceneDirective as any).object as THREE.Scene)
+      .children
+      .filter(child => child.type === 'Object3D');
+    console.log(relevantObjects);
+    const intersectedObjects = this.raycaster.intersectObjects(relevantObjects);
+    intersectedObjects.map(x => x.object.type).forEach(console.log.bind(console));
+  }
 }
